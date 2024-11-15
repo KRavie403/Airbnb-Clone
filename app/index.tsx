2572cfa751769e8570529a764dd8ea78aefa3a29
Zoe <83668266+KRavie403@users.noreply.github.com>
2024-11-15 23:20:01 +0900

    feat: add index.tsx

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Airbnb Clone</h1>
+      <div className="text-rose-500 text-2xl"> Hello Airbnb</div>
+    </div>
+  );
+};
+
+export default IndexPage;
